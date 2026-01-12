7484feda17e7ec2fdba05e3c7504353864f0339f
ShumairRafi <81961415+ShumairRafi@users.noreply.github.com>
2026-01-12 23:06:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def create_pdf_profile(data):
     content = []
 
     # ---------- HEADER (LOGO LEFT, TEXT RIGHT) ----------
-    logo = Image("assets/logo.jpg", width=130, height=90)
+    logo = Image("assets/logo.jpg", width=200, height=65)
     
     right_header = Paragraph(
         """
